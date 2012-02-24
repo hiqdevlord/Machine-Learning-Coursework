347ff232f950a3b54d2e95c37bc4c17b7e729696
hiqdevlord <hiqdevlord@gmail.com>
2012-02-24 12:13:04 +0000

    Report 2 tex

--- cw2/getSingleOutputNets.m
+++ cw2/getSingleOutputNets.m
@@ -1,18 +1,19 @@
 function nets=getSingleOutputNets(P, T)
 
 for i=1:6
+    
     Tnew=T(i,:);
-    net = feedforwardnet([30 30],'trainscg');
-    net.divideParam.trainRatio = 1;
-    net.divideParam.valRatio = 0;
-    net.divideParam.testRatio = 0.0;
+    net = feedforwardnet(35,'trainscg');
+    net.divideFcn='dividetrain';
+    %[trainInd,valInd,testInd] = dividetrain(Q,trainRatio,valRatio,testRatio)
     net = configure(net, P, Tnew); 
-    net.trainParam.epochs =150;
-    net.trainParam.lr = 0.05;
+    net.trainParam.epochs =100;
+    net.trainParam.lr = 0.005;
     net.trainParam.goal = 0.01 ;
     net.trainParam.min_grad = 0.01;
-    net.layers{1}.transferFcn = 'tansig';
     net.performFcn = 'msereg';
     net.performParam.ratio = 0.5;
+    
     nets(i).net = train(net, P, Tnew);
+
 end
